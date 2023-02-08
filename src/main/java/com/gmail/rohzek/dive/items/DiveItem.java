d4c4109fdc8e6fe6259007b7bf338364bdc6ec76
Rohzek <rohzek@gmail.com>
2023-02-08 00:54:06 -0600

    Updated to 1.19.3
    Major changes to both textures, and to creative tabs

--- src/main/java/com/gmail/rohzek/dive/items/DiveItem.java
+++ src/main/java/com/gmail/rohzek/dive/items/DiveItem.java
@@ -1,13 +1,11 @@
 package com.gmail.rohzek.dive.items;
 
-import com.gmail.rohzek.dive.main.Main;
-
 import net.minecraft.world.item.Item;
 
 public class DiveItem extends Item
 {
 	public DiveItem() 
 	{
-		super(new Item.Properties().tab(Main.DIVE_GEAR_TAB).stacksTo(1));
+		super(new Item.Properties().stacksTo(1));
 	}
 }
