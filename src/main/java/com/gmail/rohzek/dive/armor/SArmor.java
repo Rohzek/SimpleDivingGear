193bec0c81e848b77c61c3d6f60419130fa77b60
Rohzek <rohzek@gmail.com>
2023-06-27 13:27:53 -0500

    Updated to 1.19.4
    Fixed error with soundeffect not being properly registered on armor equip,
    and streamlined the checking for blocks above you when using the water diving gear

--- src/main/java/com/gmail/rohzek/dive/armor/SArmor.java
+++ src/main/java/com/gmail/rohzek/dive/armor/SArmor.java
@@ -2,22 +2,22 @@ package com.gmail.rohzek.dive.armor;
 
 import com.gmail.rohzek.dive.lib.DeferredRegistration;
 
-import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem.Type;
 import net.minecraftforge.registries.RegistryObject;
 
 public class SArmor 
 {
-	public static final RegistryObject<SDiveGear> DIVE_HELMET = DeferredRegistration.ITEMS.register("divehelmet", () -> new SDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.HEAD));
-	public static final RegistryObject<SDiveGear> DIVE_HELMET_LIGHTS = DeferredRegistration.ITEMS.register("divehelmetlighted", () -> new SDiveGear( DiveArmorMaterial.divegear, EquipmentSlot.HEAD));
-	public static final RegistryObject<SDiveGear> DIVE_CHEST = DeferredRegistration.ITEMS.register("divechest", () -> new SDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.CHEST));
-	public static final RegistryObject<SDiveGear> DIVE_LEGS = DeferredRegistration.ITEMS.register("divelegs", () -> new SDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.LEGS));
-	public static final RegistryObject<SDiveGear> DIVE_BOOTS = DeferredRegistration.ITEMS.register("diveboots", () -> new SDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.FEET));
+	public static final RegistryObject<SDiveGear> DIVE_HELMET = DeferredRegistration.ITEMS.register("divehelmet", () -> new SDiveGear(DiveArmorMaterial.divegear, Type.HELMET));
+	public static final RegistryObject<SDiveGear> DIVE_HELMET_LIGHTS = DeferredRegistration.ITEMS.register("divehelmetlighted", () -> new SDiveGear( DiveArmorMaterial.divegear, Type.HELMET));
+	public static final RegistryObject<SDiveGear> DIVE_CHEST = DeferredRegistration.ITEMS.register("divechest", () -> new SDiveGear(DiveArmorMaterial.divegear, Type.CHESTPLATE));
+	public static final RegistryObject<SDiveGear> DIVE_LEGS = DeferredRegistration.ITEMS.register("divelegs", () -> new SDiveGear(DiveArmorMaterial.divegear, Type.LEGGINGS));
+	public static final RegistryObject<SDiveGear> DIVE_BOOTS = DeferredRegistration.ITEMS.register("diveboots", () -> new SDiveGear(DiveArmorMaterial.divegear, Type.BOOTS));
 	
-	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_HELMET = DeferredRegistration.ITEMS.register("netherdivehelmet", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.HEAD));
-	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_HELMET_LIGHTS = DeferredRegistration.ITEMS.register("netherdivehelmetlighted", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.HEAD));
-	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_CHEST = DeferredRegistration.ITEMS.register("netherdivechest", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.CHEST));
-	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_LEGS = DeferredRegistration.ITEMS.register("netherdivelegs", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.LEGS));
-	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_BOOTS = DeferredRegistration.ITEMS.register("netherdiveboots", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, EquipmentSlot.FEET));
-	
+	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_HELMET = DeferredRegistration.ITEMS.register("netherdivehelmet", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, Type.HELMET));
+	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_HELMET_LIGHTS = DeferredRegistration.ITEMS.register("netherdivehelmetlighted", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, Type.HELMET));
+	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_CHEST = DeferredRegistration.ITEMS.register("netherdivechest", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, Type.CHESTPLATE));
+	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_LEGS = DeferredRegistration.ITEMS.register("netherdivelegs", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, Type.LEGGINGS));
+	public static final RegistryObject<SNetherDiveGear> NETHER_DIVE_BOOTS = DeferredRegistration.ITEMS.register("netherdiveboots", () -> new SNetherDiveGear(DiveArmorMaterial.divegear, Type.BOOTS));
+
 	public static void register() {}
 }
